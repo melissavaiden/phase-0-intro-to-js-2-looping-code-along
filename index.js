e8a58700d075d31895f9e29d28a0638edec60c54
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-12 15:11:46 -0500

    Completed Almost

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+const names = ["Chloe", "Sean", "Gabriella"];
+
+function writeCards(names, exclamation) {
+    let newNames = []
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${exclamation} gift!`);
+    }
+    return newNames;
+}
+writeCards(names);
